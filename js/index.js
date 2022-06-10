35d4a7b2428e1df37f9703cc29c107ac72856843
Ryan Hollink <ryan.hollink@hotmail.com>
2022-06-10 16:32:54 +0200

    Added extra information to generate with. Also added a new result field & refactored code.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 
 const ResultFields = {
+    Themes: document.getElementById("theme"),
     Genre: document.getElementById("genre"),
     Location: document.getElementById("location"),
     Mechanic: document.getElementById("mechanic")
@@ -8,25 +9,28 @@ const ResultFields = {
 
 const Generators = [
     {
-        Genre: ["Action", "Rhythm", "Platformer", "Stealth", "Shooter", "Horror", "Survival"],
-        Location: ["Deep Ocean", "World"],
-        Mechanic: ["Building", "Hi"]
+        Themes: ["Summer", "Winter", "Sci-Fi", "Autumn", "Spring", "Tropical", "Fantasy", "Steampunk", "Middle Ages"],
+        Genre: ["Action", "Rhythm", "Platformer", "Stealth", "Shooter", "Horror", "Survival", "Fighting", "RPG", "MMORPG", "Turn-Based", "Sports", "Social"],
+        Location: ["Deep Ocean", "Islands", "Floating Islands", "Park", "Open Ocean", "Beach", "City", "Town"],
+        Mechanic: ["Building", "Swimming", "Puzzle Solving", "Teams", "Voting", "Trivia", "Jumping", "Running", "Throwing", "Interacting", "Environment", "Speed"]
     }
 ]
 
-// Events
-document.body.onkeydown = function(e)
+// Methods
+function Generate()
 {
-    if (e.keyCode != 32) { return; }
-
     let Generator = Generators[0]
     for (let key in Generator)
     {
         console.log(Generator[key])
         let Random = Math.floor(Math.random() * Generator[key].length)
-        if (Random < 0) {Random = 0}
-        console.log(Math.random() * Generator[key].length)
-        console.log(Random)
         ResultFields[key].innerHTML = Generator[key][Random];
     }
+}
+
+// Events
+document.body.onkeydown = function(e)
+{
+    if (e.keyCode != 32) { return; }
+    Generate();
 }
